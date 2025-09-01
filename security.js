91c2224ec6d7997b444527b28c9008525d413305
tkroy02 <75790163+tkroy02@users.noreply.github.com>
2025-09-01 13:44:54 -0400

    Delete security.js

--- security.js
+++
@@ -1,7 +0,0 @@
-// security.js
-window.addEventListener('load', function() {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-99H54WVS80');
-});
